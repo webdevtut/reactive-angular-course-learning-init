2854cb4a66d7fb98bf51f0412330e718995608e2
Tushar Kadam <48817471+webdevtut@users.noreply.github.com>
2022-12-21 20:28:01 +0530

    3.35

--- src/app/services/courses.service.ts
+++ src/app/services/courses.service.ts
@@ -27,6 +27,19 @@ loadCourseById(courseId: number){
             );
 }
 
+loadAllCourseLessons(courseId: number) : Observable<Lesson[]>{
+    return this.http.get<Lesson[]>('/api/lessons',{
+        params: {
+            pageSize: "10000",
+            courseId: courseId.toString()
+        }
+    })
+    .pipe(
+        map(res => res["payload"]),
+        shareReplay()
+    );
+}
+
 loadAllCourses() {
     const loadCourses$ = this.http.get<Course[]>('/api/courses')
         .pipe(
